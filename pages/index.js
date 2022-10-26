3c33d3d74ad5a8f3a5d7757b458045f06684bed2
Isabela Ceballos <isaceballos2001@gmail.com>
2022-10-25 20:02:20 -0500

    page-welcome

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
-import { HOLA__div, TEXT__p} from './estilos.js';
+import Image from 'next/image';
+import libro from '../src/assets/logo.png';
+import logoPrincipal from '../src/assets/logo_welcome.svg'
+import logoGoogle from '../src/assets/google.svg';
+
+import { PRINCIPAL__div,
+        TITLE__p, 
+        CONTENTP__div, 
+        CONTENTIMAGE__div, 
+        CONTENTBUTTON__div} from './estilos.js';
 
 function HomePage() {
     return(
-        <>
-            <HOLA__div>HandBook</HOLA__div>
-            <TEXT__p>esto es text</TEXT__p>
-        </> 
+        <PRINCIPAL__div>
+                <TITLE__p>HandBook</TITLE__p>
+            <CONTENTP__div>
+                <p>Dale otra vida a tus libros. Inicia ahora y encuentra tu próxima aventura literaria.</p>
+            </CONTENTP__div>
+            <CONTENTIMAGE__div>
+                <Image src={logoPrincipal} alt='logo_app'/>
+            </CONTENTIMAGE__div>
+            <CONTENTBUTTON__div>
+                <Image src={logoGoogle} height={20} width={20} />
+                Inicia con Google
+            </CONTENTBUTTON__div>
+        </PRINCIPAL__div> 
     )
 }
 
