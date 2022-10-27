5dd7d0764979c6702d17b97e9369f9d73b42811a
Daniela Jurado Blandón <daniijuradob@gmail.com>
2022-10-26 21:24:28 -0500

    Custom icons

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,4 +1,9 @@
 import Link from 'next/link';
+import { HomeIcon } from '../../assets/HomeIcon';
+import { ProfileIcon } from '../../assets/ProfileIcon';
+import { AddIcon } from '../../assets/AddIcon';
+import { CommunityIcon } from '../../assets/CommunityIcon';
+import { EventIcon } from '../../assets/EventsIcon';
 
 /*Syled Components */
 import { CONTAINER__nav } from './styles';
@@ -7,12 +12,11 @@ export const Menu = () => {
     return (
         <>
             <CONTAINER__nav>
-                <Link href="/login"><a>L</a></Link>
-                <Link href="/home"><a>H</a></Link>
-                <Link href="/profile"><a>P</a></Link>
-                <Link href="/newBook"><a>NB</a></Link>
-                <Link href="/communities"><a>C</a></Link>
-                <Link href="/events"><a>E</a></Link>
+                <Link href="/home"><a><HomeIcon fill="transparent"/></a></Link>
+                <Link href="/profile"><a><ProfileIcon fill="transparent"/></a></Link>
+                <Link href="/newBook"><a><AddIcon fill="transparent"/></a></Link>
+                <Link href="/communities"><a><CommunityIcon fill="transparent"/></a></Link>
+                <Link href="/events"><a><EventIcon fill="transparent"/></a></Link>
             </CONTAINER__nav>
         </>
     )
